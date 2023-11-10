a1c03eef410d20ede47b11dcb127e19a7c08f591
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-11-10 13:13:15 +0100

    'udate'

--- JGmain.h
+++ JGmain.h
@@ -19,9 +19,9 @@ void JGread(char *command, size_t size);
 void JGexecute(const char *command);
 void JGexit(int status);
 void JGprint(const char *message);
-<<<<<<< HEAD
+
 int _putchar(char c);
-=======
+
 void JGcd(const char *path);
 void JGexit(int status);
 void JGhelp(void);
