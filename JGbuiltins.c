6ac8795ddc256f367f371712cef9c1c195e970d1
Kurrgat <mrcykj@gmail.com>
2023-11-10 02:46:36 -0800

    updates

---
+++ JGbuiltins.c
@@ -0,0 +1,63 @@
+#include "JGmain.h"
+
+/**
+ * JGcd - Change current working directory
+ * @path: The path to change to
+ */
+void JGcd(const char *path)
+{
+	if (path == NULL)
+	{
+		JGprint("cd: missing argument\n");
+	}
+	else
+	{
+		if (chdir(path) != 0)
+		{
+			perror("cd");
+		}
+	}
+}
+
+/**
+ * JGexit - Exit the shell
+ * @status: The exit status
+ */
+void JGexit(int status)
+{
+	exit(status);
+}
+
+/**
+ * JGhelp - Display help information
+ */
+void JGhelp(void)
+{
+	JGprint("Built-in commands:\n");
+	JGprint("cd [directory] - Change the current working directory\n");
+	JGprint("echo [arguments] - Print arguments to the standard output\n");
+	JGprint("exit - Exit the shell\n");
+	JGprint("help - Display this help message\n");
+}
+
+/**
+ * handle_builtin - Execute built-in commands
+ * @command: The command to check
+ */
+void handle_builtin(const char *command)
+{
+	if (strcmp(command, "cd") == 0)
+	{
+		const char *path = command + 3;
+
+		JGcd(path);
+	}
+	else if (strcmp(command, "exit") == 0)
+	{
+		JGexit(EXIT_SUCCESS);
+	}
+	else if (strcmp(command, "help") == 0)
+	{
+		JGhelp();
+	}
+}
---
+++ JGenvironment.c
@@ -0,0 +1,47 @@
+#include "JGmain.h"
+
+/**
+ * JGprint_environment - Print environment variables
+ */
+void JGprint_environment(const char *env[])
+{
+	int i = 0;
+
+	while (env[i] != NULL)
+	{
+		JGprint(env[i]);
+		JGprint("\n");
+		i++;
+	}
+}
+
+/**
+ * JGset_environment - Set a new environment variable
+ * @shell: Shell structure containing environment variables
+ * @variable: The name of the variable
+ * @value: The value to set
+ */
+void JGset_environment(Shell *shell, const char *variable, const char *value)
+{
+	if (setenv(variable, value, 1) != 0)
+	{
+		perror("setenv");
+	}
+
+	shell->env = environ;
+}
+
+/**
+ * JGunset_environment - Unset an environment variable
+ * @shell: Shell structure containing environment variables
+ * @variable: The name of the variable to unset
+ */
+void JGunset_environment(Shell *shell, const char *variable)
+{
+	if (unsetenv(variable) != 0)
+	{
+		perror("unsetenv");
+	}
+
+	shell->env = environ;
+}
